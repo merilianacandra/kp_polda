41cde204d99ecf9a713dc7487de16be04251ec71
soniaginasari Admin <soniaginasari67@gmail.com>
2020-07-27 04:38:53 +0800

    Merge branch 'master' of https://github.com/merilianacandra/kp_polda
    # Conflicts:
    #	app/build.gradle

---
+++ app/src/main/java/com/example/myapplication/SplashscreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashscreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashscreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000L);
+    }
+
+}
