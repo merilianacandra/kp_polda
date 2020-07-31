d8baabe529f3058f1e468839db14a8161b94cbc4
soniaginasari Admin <soniaginasari67@gmail.com>
2020-07-31 15:06:20 +0800

    Merge branch 'master' of https://github.com/merilianacandra/kp_polda
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.GridLayout;
@@ -32,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void surat_masuk(View view) {
+        Intent intent = new Intent(MainActivity.this, SuratMasukActivity.class);
+        startActivity(intent);
+    }
+
+    public void surat_keluar(View view) {
+        Intent intent = new Intent(MainActivity.this, SuratKeluarActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/SuratMasukActivity.java
+++ app/src/main/java/com/example/myapplication/SuratMasukActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SuratMasukActivityActivity extends AppCompatActivity {
+public class SuratMasukActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
